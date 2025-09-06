96936c1bfdf0b45af5f2530862565349fdcd82d2
Luis Galea <luisgalea31@gmail.com>
2025-09-05 23:12:12 -0400

    feat<capy-quest-app>: create index of assets nfts

---
+++ capy-quest-app/src/assets/NFTs/index.ts
@@ -0,0 +1,13 @@
+import BabyCapy from './BabyCapy.png'
+import ExploreCapy from './ExploreCapy.png'
+import GlodenCapy from './GoldenCapy.png'
+import LegendaryCapy from './LegendaryCapy.png'
+import WiseCapy from './WiseCapy.png'
+
+export {
+  BabyCapy,
+  ExploreCapy,
+  WiseCapy,
+  LegendaryCapy,
+  GlodenCapy
+}
